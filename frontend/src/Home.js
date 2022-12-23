941641b032f7c2a5669864538c3d853bcd4998a9
Sam <sam.pierce76@gmail.com>
2022-12-22 21:31:40 -0500

    Resolved bug with home elements transform; bug with index.html copied before frontend build; added spring security

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,10 +4,10 @@ import "./Home.css";
 import AppNavbar from "./AppNavbar";
 import { Link } from "react-router-dom";
 import { Button, styled, Tooltip } from "@mui/material";
-import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import $ from 'jquery';
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import $ from "jquery";
 
 const OvalButton = styled(Button)(({ theme }) => ({
   boxShadow: "none",
@@ -40,57 +40,91 @@ const OvalButton = styled(Button)(({ theme }) => ({
   },
 }));
 
-const StyledDiv = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main
+const StyledDiv = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
 }));
 
 const getTriangle = () => {
+  let width =
+    $("#vehicle-button").position().left - $("#task-button").position().left;
+  let height =
+    $("#task-button").position().top - $("#vehicle-button").position().top;
+  let hypotenuse = Math.round(
+    Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2))
+  );
 
-  let width = $("#vehicle-button").position().left - $("#task-button").position().left;
-  let height = $("#task-button").position().top - $("#vehicle-button").position().top;
-  let hypotenuse = Math.round(Math.sqrt((Math.pow(width, 2)) + (Math.pow(height, 2))));
-
-  // horizontal shift at line end caused by rotation = (1 - cos(angle)) * 1/2 length
-  // cos(angle) = width/hypotenuse
-  const originX = $("#vehicle-button").position().left + ($("#vehicle-button").width() / 2);
-  let rotateOffsetX = (-($("#line1").width() / 2) * (1 - (width / hypotenuse)));
-  let buttonOffsetX = (-originX + $("#task-button").position().left + ($("#task-button").width() / 2))
-  let offsetX = rotateOffsetX + buttonOffsetX
-
-  // vertical shift at line end caused by rotation = sin(angle) * 1/2 length
-  // sin(angle) = height / hypotenuse
-  let rotateOffsetY = (-($("#line1").width() / 2) * (height / hypotenuse))
-  let buttonOffsetY = ($("#task-button").height() / 2);
-  let offsetY = rotateOffsetY + buttonOffsetY
-
+  // line widths need to be calculated before offsets, otherwise position will be off on initial page load
   $("#line1").width(hypotenuse);
-  $("#line1").css({ 'transform': 'translate(' + offsetX + 'px, ' + offsetY + 'px) rotate(' + -Math.atan(height / width) + 'rad)' });
+  $("#line2").width(hypotenuse);
 
   // horizontal shift at line end caused by rotation = (1 - cos(angle)) * 1/2 length
   // cos(angle) = width/hypotenuse
-  rotateOffsetX = (-($("#line2").width() / 2) * (1 - (width / hypotenuse)));
-  buttonOffsetX = (-originX) + $("#vehicle-button").position().left + ($("#vehicle-button").width() / 2)
-  offsetX = rotateOffsetX + buttonOffsetX
+  const originX =
+    $("#vehicle-button").position().left + $("#vehicle-button").width() / 2;
+  let rotateOffsetX = -($("#line1").width() / 2) * (1 - width / hypotenuse);
+  let buttonOffsetX =
+    -originX +
+    $("#task-button").position().left +
+    $("#task-button").width() / 2;
+  let offsetX = rotateOffsetX + buttonOffsetX;
 
   // vertical shift at line end caused by rotation = sin(angle) * 1/2 length
   // sin(angle) = height / hypotenuse
-  rotateOffsetY = (-($("#line2").width() / 2) * (height / hypotenuse));
-  buttonOffsetY = ($("#schedule-button").height() / 2)
-  offsetY = rotateOffsetY + buttonOffsetY
+  let rotateOffsetY = -($("#line1").width() / 2) * (height / hypotenuse);
+  let buttonOffsetY = $("#task-button").height() / 2;
+  let offsetY = rotateOffsetY + buttonOffsetY;
 
-  $("#line2").width(hypotenuse);
-  $("#line2").css({ 'transform': 'translate(' + offsetX + 'px, ' + offsetY + 'px) rotate(' + Math.atan(height / width) + 'rad)' })
+  $("#line1").css({
+    transform:
+      "translate(" +
+      offsetX +
+      "px, " +
+      offsetY +
+      "px) rotate(" +
+      -Math.atan(height / width) +
+      "rad)",
+  });
 
-  $("#line3").width($("#schedule-button").position().left - $("#task-button").position().left)
-  $("#line3").css({ 'transform': 'translate(-50%, ' + $("#schedule-button").height() / 2 + 'px)' })
-}
+  // horizontal shift at line end caused by rotation = (1 - cos(angle)) * 1/2 length
+  // cos(angle) = width/hypotenuse
+  rotateOffsetX = -($("#line2").width() / 2) * (1 - width / hypotenuse);
+  buttonOffsetX =
+    -originX +
+    $("#vehicle-button").position().left +
+    $("#vehicle-button").width() / 2;
+  offsetX = rotateOffsetX + buttonOffsetX;
 
+  // vertical shift at line end caused by rotation = sin(angle) * 1/2 length
+  // sin(angle) = height / hypotenuse
+  rotateOffsetY = -($("#line2").width() / 2) * (height / hypotenuse);
+  buttonOffsetY = $("#schedule-button").height() / 2;
+  offsetY = rotateOffsetY + buttonOffsetY;
+
+  $("#line2").css({
+    transform:
+      "translate(" +
+      offsetX +
+      "px, " +
+      offsetY +
+      "px) rotate(" +
+      Math.atan(height / width) +
+      "rad)",
+  });
+
+  $("#line3").width(
+    $("#schedule-button").position().left - $("#task-button").position().left
+  );
+  $("#line3").css({
+    transform: "translate(-50%, " + $("#schedule-button").height() / 2 + "px)",
+  });
+};
 
 class Home extends Component {
   componentDidMount() {
-    getTriangle()
+    getTriangle();
+
     $(window).resize(function () {
-      getTriangle()
+      getTriangle();
     });
   }
 
@@ -101,9 +135,21 @@ class Home extends Component {
         <StyledDiv id="line1" className="line" />
         <StyledDiv id="line2" className="line" />
         <StyledDiv id="line3" className="line" />
-        <Tooltip title="Vehicles" placement="bottom-start"><OvalButton id="vehicle-button" component={Link} to="/vehicles"><DirectionsCarIcon fontSize="large" className="icon" /></OvalButton></Tooltip>
-        <Tooltip title="Maintenance Tasks" placement="bottom-start"><OvalButton id="task-button" component={Link} to="/tasks"><AssignmentIcon fontSize="large" className="icon" /></OvalButton></Tooltip>
-        <Tooltip title="Schedules" placement="bottom-start"><OvalButton id="schedule-button" component={Link} to="/schedules"><CalendarMonthIcon fontSize="large" className="icon" /></OvalButton></Tooltip>
+        <Tooltip title="Vehicles" placement="bottom-start">
+          <OvalButton id="vehicle-button" component={Link} to="/vehicles">
+            <DirectionsCarIcon fontSize="large" className="icon" />
+          </OvalButton>
+        </Tooltip>
+        <Tooltip title="Maintenance Tasks" placement="bottom-start">
+          <OvalButton id="task-button" component={Link} to="/tasks">
+            <AssignmentIcon fontSize="large" className="icon" />
+          </OvalButton>
+        </Tooltip>
+        <Tooltip title="Schedules" placement="bottom-start">
+          <OvalButton id="schedule-button" component={Link} to="/schedules">
+            <CalendarMonthIcon fontSize="large" className="icon" />
+          </OvalButton>
+        </Tooltip>
       </div>
     );
   }
